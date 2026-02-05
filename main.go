a14eaee75ff1a4516cf39b3dae5c0bff2deb8792
fazrinanabillah <fazrinabillah@gmail.com>
2026-02-05 14:42:52 +0700

    Feat: Concurrent downloader

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -28,6 +29,7 @@ func DownloadFile(url, destDir string) error {
 
 	resp, err := http.Get(url)
 	if err != nil {
+		_ = os.Remove(filePath)
 		return err
 	}
 
@@ -35,6 +37,7 @@ func DownloadFile(url, destDir string) error {
 
 	// Check for server response
 	if resp.StatusCode != http.StatusOK {
+		_ = os.Remove(filePath)
 		return fmt.Errorf("request failed: %s", resp.Status)
 	}
 
@@ -66,13 +69,107 @@ func SequentialDownloader(urls []string, destDir string) error {
 	return nil
 }
 
+type Result struct {
+	URL      string
+	FileName string
+	Size     int64
+	Duration time.Duration
+	Error    error
+}
+
+func ConcurrentDownloader(urls []string, destDir string, maxConcurrent int) error {
+	if err := os.MkdirAll(destDir, 0755); err != nil {
+		return err
+	}
+
+	results := make(chan Result)
+	limiter := make(chan struct{}, maxConcurrent)
+	var wg sync.WaitGroup
+
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+
+			limiter <- struct{}{}
+			defer func() { <-limiter }()
+
+			start := time.Now().UTC()
+			fileName := filepath.Base(url)
+			filePath := filepath.Join(destDir, fileName)
+
+			out, err := os.Create(filePath)
+			if err != nil {
+				results <- Result{URL: url, Error: err}
+				return
+			}
+			defer out.Close()
+
+			resp, err := http.Get(url)
+			if err != nil {
+				_ = os.Remove(filePath)
+				results <- Result{URL: url, Error: err}
+				return
+			}
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				_ = os.Remove(filePath)
+				results <- Result{URL: url, Error: fmt.Errorf("status: %s", resp.Status)}
+				return
+			}
+
+			size, err := io.Copy(out, resp.Body)
+			if err != nil {
+				_ = os.Remove(filePath)
+				results <- Result{URL: url, Error: err}
+				return
+			}
+
+			timeSince := time.Since(start)
+			results <- Result{URL: url, FileName: fileName, Size: size, Duration: timeSince, Error: nil}
+
+		}(url)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	var totalSize int64
+	var errors []error
+	start := time.Now().UTC()
+
+	for result := range results {
+		if result.Error != nil {
+			fmt.Printf("Error downloading %s: %s\n", result.URL, result.Error.Error())
+			errors = append(errors, result.Error)
+		} else {
+			totalSize += result.Size
+			fmt.Printf("Downloaded %s (%d bytes) in %s\n", result.FileName, result.Size, result.Duration)
+		}
+	}
+
+	startedSince := time.Since(start)
+	fmt.Printf("All downloads completed in %s, Total: %d bytes\n", startedSince, totalSize)
+
+	if len(errors) > 0 {
+		return fmt.Errorf("error downloading: %+v", errors)
+	}
+
+	return nil
+}
+
 func main() {
 	urls := []string{
+		"https://file-examples.com/storage/feb18787c869840dc97160c/2017/10/file_example_JPG_1MB.jpg",
 		"https://www.w3.org/WAI/WCAG21/Techniques/pdf/img/table-word.jpg",
 		"https://go.dev/images/go-logo-blue.svg",
 	}
 
-	if err := SequentialDownloader(urls, "./"); err != nil {
+	if err := ConcurrentDownloader(urls, "./", 3); err != nil {
 		slog.Error(err.Error())
 		return
 	}
